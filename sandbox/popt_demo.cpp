996739d425e0b11027721625855c7c55c72c944c
Bijou <achrononmaster@gmail.com>
2019-03-10 01:10:25 +1300

    Added -n |--nsteps <int> option to popt demo.

--- sandbox/popt_demo.cpp
+++ sandbox/popt_demo.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <popt.h>
+#include <string.h>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ struct goodwinParams {
     double b;
     double w0;
     double Y0;
+    int Nsteps;
 };
 
 static void parseArguments( int argc, const char **argv, goodwinParams* gparams )
@@ -26,29 +28,42 @@ static void parseArguments( int argc, const char **argv, goodwinParams* gparams 
     poptContext optCon;
     const struct poptOption optionsTable[] = {
         POPT_AUTOHELP
-        { "r", 'r', POPT_ARG_DOUBLE, &gparams->r, 0,
-            " Set wage appreciation parameter." },
-        { "c", 'c', POPT_ARG_DOUBLE, &gparams->c, 0,
+        { "nsteps  ", 'n', POPT_ARG_INT, &gparams->Nsteps, 0,
+            "Set number of time steps." },
+        { "r  ", 'r', POPT_ARG_DOUBLE, &gparams->r, 0,
+            "Set wage appreciation parameter." },
+        { "c  ", 'c', POPT_ARG_DOUBLE, &gparams->c, 0,
             "Set wage growth decay rate parameter." },
-        { "a", 'a', POPT_ARG_DOUBLE, &gparams->a, 0,
+        { "a  ", 'a', POPT_ARG_DOUBLE, &gparams->a, 0,
             "Set output growth rate parameter." },
-        { "b", 'b', POPT_ARG_DOUBLE, &gparams->b, 0,
+        { "b  ", 'b', POPT_ARG_DOUBLE, &gparams->b, 0,
             "Set output depreciation parameter." },
-        { "w0", 'w', POPT_ARG_DOUBLE, &gparams->w0, 0,
+        { "w0 ", 'w', POPT_ARG_DOUBLE, &gparams->w0, 0,
             "Set initial wage share." },
-        { "Y0", 'y', POPT_ARG_DOUBLE, &gparams->Y0, 0,
+        { "Y0 ", 'y', POPT_ARG_DOUBLE, &gparams->Y0, 0,
             "Set initial output level." },
-        { "Y0", 'Y', POPT_ARG_DOUBLE, &gparams->Y0, 0,
+        { "Y0 ", 'Y', POPT_ARG_DOUBLE, &gparams->Y0, 0,
             "Set initial output level." },
-        {NULL, 0, 0, NULL, 0}
+        {NULL, 0, 0, NULL, 0, }
     };
-    
+    int i;
     int err;
     const char *arg = NULL;
     int argcnt = 0;
     
     optCon = poptGetContext(PROGRAM_NAME, argc, argv, optionsTable, 0);
     poptReadDefaultConfig(optCon, 0);
+    
+    /*
+     You'd need a two pass loop here to pretty print a longer program 
+     description to wrap it in 80 char width. 
+    */
+    for( i=0; i < argc; i++) {
+        //if ( strncmp( *(argv+i), "--help", 6)==0 || strncmp( *(argv+i), "-h", 2)==0 ) {
+        if ( strncmp( *(argv+i), "--help", 6)==0  ) {
+            printf("Demo use of opt for argument parsing.\n");
+        }
+    }
     
     err = poptGetNextOpt(optCon);
     if (err != -1) {
@@ -77,6 +92,7 @@ int main( int argc, const char *argv[] ) {
     gparams.b = 1.0;
     gparams.w0 = 1.0;
     gparams.Y0 = 1.0;
+    gparams.Nsteps = 100;
     parseArguments( argc, argv, &gparams );
     
     printf ("r = %f, c = %f, a = %f, b = %f, w0 = %f, Y0 = %f\n",
